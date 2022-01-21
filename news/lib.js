425d2e98575c1cd1b7f5a122752821a63fc46206
Antonio Moses <tonymoses@texasbluesalley.com>
2022-01-20 19:33:52 -0500

    adds jsdoc

--- news/lib.js
+++ news/lib.js
@@ -1,15 +1,18 @@
 const axios = require( "axios" )
 const cheerio = require( "cheerio" )
+
+/**
+ *  Uses source's address to get html to scrape. Then scrapes for data using source instance 'getArticles' function
+ * @param {NewSource} source Object representing specific news source
+ * @returns {Array} articles
+ */
  const gatherWebData = function ( source ) {
   try {
     return axios.get( source.address )
       .then( ( response ) => {
         console.log( `Loading (${source.proper}) Data from ${source.address}` )
-
         const html = response.data
-        // const $ = cheerio.load( html )
         try {
-
           const src_articles = source.getArticles( html )
           console.log( 'gather articles: ', src_articles.length )
           return src_articles
